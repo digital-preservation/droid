35484f1ae308c11a47cec5c36d8777af9d368ca8
rbrennan <rbrennan@8cb85be0-0016-d946-ad3d-90027f349634>
2012-05-30 12:12:44 +0000

    New test for Reverse Scan bug
    git-svn-id: https://wb-d-tfs2.web.local/svn/dp/trunk/droid@51 8cb85be0-0016-d946-ad3d-90027f349634

---
+++ droid-core/src/test/java/uk/gov/nationalarchives/droid/core/ReverseScanBugFixTest.java
@@ -0,0 +1,65 @@
+/*
+ * Test byteseek handles senior bit of ~bitmask correctly
+ */
+package uk.gov.nationalarchives.droid.core;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.List;
+import org.junit.*;
+import static org.junit.Assert.*;
+import uk.gov.nationalarchives.droid.core.interfaces.IdentificationRequest;
+import uk.gov.nationalarchives.droid.core.interfaces.IdentificationResult;
+import uk.gov.nationalarchives.droid.core.interfaces.IdentificationResultCollection;
+import uk.gov.nationalarchives.droid.core.interfaces.RequestIdentifier;
+import uk.gov.nationalarchives.droid.core.interfaces.resource.FileSystemIdentificationRequest;
+import uk.gov.nationalarchives.droid.core.interfaces.resource.RequestMetaData;
+
+/**
+ *
+ * @author rbrennan
+ */
+public class ReverseScanBugFixTest {
+    
+    private final String TESTAREA = "test_sig_files/";
+    private final String SIGFILE = "ReverseScan_SignatureFile.xml";
+    private final String SCANFILE = "ReverseScan.enl";
+
+    private final int EXPECTED_HITS = 0;         // change to 1 for byteseek 1.2
+    private final String EXPECTED_PUID = "dev/2";
+
+    @Test
+    public void testAnyBitmask() throws Exception {
+    
+        BinarySignatureIdentifier droid = new BinarySignatureIdentifier();
+        droid.setSignatureFile(TESTAREA + SIGFILE);
+        try {
+            droid.init();
+        } catch (SignatureParseException x) {
+            assertEquals("Can't parse signature file", x.getMessage());
+        }
+        File file = new File(TESTAREA + SCANFILE);
+        assertTrue(file.exists());
+        URI resourceUri = file.toURI();
+  
+        InputStream in = new FileInputStream(file);
+        RequestMetaData metaData = new RequestMetaData(file.length(), file.lastModified(), SCANFILE);
+        RequestIdentifier identifier = new RequestIdentifier(resourceUri);
+        identifier.setParentId(1L);
+        
+        IdentificationRequest request = new FileSystemIdentificationRequest(metaData, identifier);
+        request.open(in);
+
+        IdentificationResultCollection resultsCollection = droid.matchBinarySignatures(request);
+        List<IdentificationResult> results = resultsCollection.getResults();
+        
+        assertEquals(EXPECTED_HITS, results.size());
+        
+        while (results.iterator().hasNext()) {
+            IdentificationResult result = results.iterator().next();
+            assertEquals(EXPECTED_PUID, result.getPuid());
+        }
+      }
+}
