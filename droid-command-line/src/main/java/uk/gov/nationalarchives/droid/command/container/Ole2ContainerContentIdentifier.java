2ccd59e84d1958612d3e76a1e5d9969ef40d806b
rbrennan <rbrennan@8cb85be0-0016-d946-ad3d-90027f349634>
2012-07-02 17:26:03 +0000

    [feature] Command Line (no profile) handling temp file location (work in progress...)
    git-svn-id: https://wb-d-tfs2.web.local/svn/dp/trunk/droid@106 8cb85be0-0016-d946-ad3d-90027f349634

--- droid-command-line/src/main/java/uk/gov/nationalarchives/droid/command/container/Ole2ContainerContentIdentifier.java
+++ droid-command-line/src/main/java/uk/gov/nationalarchives/droid/command/container/Ole2ContainerContentIdentifier.java
@@ -4,16 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import uk.gov.nationalarchives.droid.container.ContainerFileIdentificationRequest;
 import uk.gov.nationalarchives.droid.container.ContainerSignatureMatch;
 import uk.gov.nationalarchives.droid.container.ContainerSignatureMatchCollection;
 import uk.gov.nationalarchives.droid.container.FileFormatMapping;
-import uk.gov.nationalarchives.droid.core.interfaces.IdentificationMethod;
-import uk.gov.nationalarchives.droid.core.interfaces.IdentificationRequest;
-import uk.gov.nationalarchives.droid.core.interfaces.IdentificationResultImpl;
-import uk.gov.nationalarchives.droid.core.interfaces.RequestIdentifier;
+import uk.gov.nationalarchives.droid.core.interfaces.*;
 
 /**
  *
@@ -22,71 +20,50 @@ import uk.gov.nationalarchives.droid.core.interfaces.RequestIdentifier;
 public class Ole2ContainerContentIdentifier extends AbstractContainerContentIdentifier {
     
     private IdentificationRequest request;
-    //private RequestMetaData requestMetaData;
-    //private RequestIdentifier requestIdentifier;
-    //private List<FileFormatMapping> fileFormatMapping;
-    
-    
+    private Map<String, IdentificationResult> puidMap = new HashMap<String, IdentificationResult>();
     
     @Override
-    public void process (File file, File tmpDir) throws IOException {
-        System.out.println("......got OLE2");
+    public void process (File file, String filePuid, File tmpDir) throws IOException {
         
-        URI resourceUri = file.toURI();
-
-        //RequestMetaData metaData = new RequestMetaData(file.length(),
-          //       file.lastModified(), file.getName());
-        RequestIdentifier identifier = new RequestIdentifier(resourceUri);
-        identifier.setParentId(1L);
         request = new ContainerFileIdentificationRequest(tmpDir);
-        System.out.println("......got request");
         
         InputStream in = null;
         try {
             in = new FileInputStream(file);
         
             request.open(in);
-            System.out.println("......opened request");
             
             int maxBytesToScan = -1; //TODO dont hardcode this either! 
             ContainerSignatureMatchCollection matches =
-                new ContainerSignatureMatchCollection(getContainerIdentifierInit().getContainerSignatures(), getContainerIdentifierInit().getUniqueFileEntries(), maxBytesToScan);
-        
+                new ContainerSignatureMatchCollection(getContainerIdentifierInit().getContainerSignatures(),
+                    getContainerIdentifierInit().getUniqueFileEntries(), maxBytesToScan);
         
             getIdentifierEngine().process(request, matches);
-            System.out.println("......ran request");
         
-            //fileFormatMapping = defs.getFormats();
-            
-            
-            
-            //IdentificationResultCollection results = new IdentificationResultCollection(request);
+            puidMap.clear();        
             for (ContainerSignatureMatch match : matches.getContainerSignatureMatches()) {
                 if (match.isMatch()) {
-                    
-                    //ContainerSignature conSig = match.getSignature();
-                    
                     List<FileFormatMapping> mappings = getFormats().get(match.getSignature().getId());
                     for (FileFormatMapping mapping : mappings) {
                         IdentificationResultImpl result = new IdentificationResultImpl();
                         result.setMethod(IdentificationMethod.CONTAINER);
                         result.setRequestMetaData(request.getRequestMetaData());
+                        String puid = mapping.getPuid();
                         result.setPuid(mapping.getPuid());
-                        //results.addResult(result);
-                        System.out.println(file.getAbsolutePath() + "," + mapping.getPuid() );
+                        if (!puidMap.containsKey(puid)) {
+                            puidMap.put(puid, result);
+                            System.out.println(file.getAbsolutePath() + "," + puid + ",OLE2 container");
+                        }
                     }
                 }
             }
-            
-            System.out.println("......finished request");
             request.close();
+            if (puidMap.isEmpty()) {
+                System.out.println(file.getAbsolutePath() + "," + filePuid +",OLE2 container not further identified");
+            }
         } finally {
             if (in != null) {
-                //try {
-                    in.close();
-                //} catch (IOException e) {
-                //    throw new CommandExecutionException(e);
-                //}
+                in.close();
             }
         }
     }
