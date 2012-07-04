a9f8df1b2bf38e6019c68c22d3be39b36a3dbe60
rbrennan <rbrennan@8cb85be0-0016-d946-ad3d-90027f349634>
2012-07-04 10:40:40 +0000

    [feature] Command Line (no profile) container handling - refactored common code
    git-svn-id: https://wb-d-tfs2.web.local/svn/dp/trunk/droid@117 8cb85be0-0016-d946-ad3d-90027f349634

--- droid-command-line/src/main/java/uk/gov/nationalarchives/droid/command/container/AbstractContainerContentIdentifier.java
+++ droid-command-line/src/main/java/uk/gov/nationalarchives/droid/command/container/AbstractContainerContentIdentifier.java
@@ -1,12 +1,17 @@
 package uk.gov.nationalarchives.droid.command.container;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import uk.gov.nationalarchives.droid.container.ContainerIdentifierInit;
-import uk.gov.nationalarchives.droid.container.ContainerSignatureDefinitions;
-import uk.gov.nationalarchives.droid.container.FileFormatMapping;
-import uk.gov.nationalarchives.droid.container.IdentifierEngine;
+import uk.gov.nationalarchives.droid.container.*;
+import uk.gov.nationalarchives.droid.core.interfaces.IdentificationMethod;
+import uk.gov.nationalarchives.droid.core.interfaces.IdentificationRequest;
+import uk.gov.nationalarchives.droid.core.interfaces.IdentificationResultCollection;
+import uk.gov.nationalarchives.droid.core.interfaces.IdentificationResultImpl;
 
 /**
  *
@@ -39,5 +44,48 @@ public abstract class AbstractContainerContentIdentifier implements ContainerCon
 
     public Map<Integer, List<FileFormatMapping>> getFormats() {
         return formats;
+    }
+    
+    @Override
+    public IdentificationResultCollection process (File file, IdentificationResultCollection containerResults) throws IOException {
+        final IdentificationRequest request = new ContainerFileIdentificationRequest(null);
+
+        InputStream in = null;
+        try {
+            in = new FileInputStream(file);
+        
+            request.open(in);
+            
+            int maxBytesToScan = -1;
+            ContainerSignatureMatchCollection matches =
+                new ContainerSignatureMatchCollection(getContainerIdentifierInit().getContainerSignatures(),
+                    getContainerIdentifierInit().getUniqueFileEntries(), maxBytesToScan);
+        
+            getIdentifierEngine().process(request, matches);
+        
+            final Map<String, String> puidMap = new HashMap<String, String>();      
+            for (ContainerSignatureMatch match : matches.getContainerSignatureMatches()) {
+                if (match.isMatch()) {
+                    List<FileFormatMapping> mappings = getFormats().get(match.getSignature().getId());
+                    for (FileFormatMapping mapping : mappings) {
+                        IdentificationResultImpl result = new IdentificationResultImpl();
+                        result.setMethod(IdentificationMethod.CONTAINER);
+                        result.setRequestMetaData(request.getRequestMetaData());
+                        String puid = mapping.getPuid();
+                        result.setPuid(mapping.getPuid());
+                        if (!puidMap.containsKey(puid)) {
+                            puidMap.put(puid, "");
+                            containerResults.addResult(result);
+                        }
+                    }
+                }
+            }
+            request.close();
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+        return containerResults;
     }
 }
