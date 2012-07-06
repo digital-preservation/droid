42f88243208d79a6cccf6e3b1098c2d0d1afc3b3
rbrennan <rbrennan@8cb85be0-0016-d946-ad3d-90027f349634>
2012-07-06 10:51:49 +0000

    Command Line help more structured.
    git-svn-id: https://wb-d-tfs2.web.local/svn/dp/trunk/droid@120 8cb85be0-0016-d946-ad3d-90027f349634

--- droid-command-line/src/main/java/uk/gov/nationalarchives/droid/command/action/HelpCommand.java
+++ droid-command-line/src/main/java/uk/gov/nationalarchives/droid/command/action/HelpCommand.java
@@ -9,8 +9,10 @@
 package uk.gov.nationalarchives.droid.command.action;
 
 import java.io.PrintWriter;
+import java.util.Comparator;
 
 import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
 
 import static uk.gov.nationalarchives.droid.command.i18n.I18N.getResource;
 
@@ -42,7 +44,35 @@ public class HelpCommand implements DroidCommand {
     @Override
     public void execute() {
         HelpFormatter formatter = new HelpFormatter();
+/*        formatter.setOptionComparator(
+            new Comparator() {
+                public int compare(Object o1, Object o2) {
+                    Option opt1 = (Option)o1; 
+                    Option opt2 = (Option)o2; 
+                    return opt2.getKey().compareToIgnoreCase(opt1.getKey()); 
+                }
+        }); */
         formatter.printHelp(writer, WRAP_WIDTH, USAGE, 
-                getResource(I18N.OPTIONS_HEADER), CommandLineParam.options(), 2, 2, null);
+                getResource(I18N.OPTIONS_HEADER), CommandLineParam.singleOptions(), 2, 3, null);
+        formatter.printOptions(writer, WRAP_WIDTH,
+                CommandLineParam.getOptions(CommandLineParam.RUN_NO_PROFILE), 2, 2);
+        formatter.printOptions(writer, WRAP_WIDTH,
+                CommandLineParam.noProfileRunSubOptions(), 5, 3);
+        formatter.printOptions(writer, WRAP_WIDTH,
+                CommandLineParam.getOptions(CommandLineParam.RUN_PROFILE), 2, 2);
+        formatter.printOptions(writer, WRAP_WIDTH,
+                CommandLineParam.profileRunSubOptions(), 5, 7);
+        formatter.printOptions(writer, WRAP_WIDTH,
+                CommandLineParam.getOptions(CommandLineParam.EXPORT_ONE_ROW_PER_FILE), 2, 9);
+        formatter.printOptions(writer, WRAP_WIDTH,
+                CommandLineParam.exportSubOptions(), 5, 7);
+        formatter.printOptions(writer, WRAP_WIDTH,
+                CommandLineParam.getOptions(CommandLineParam.EXPORT_ONE_ROW_PER_FORMAT), 2, 7);
+        formatter.printOptions(writer, WRAP_WIDTH,
+                CommandLineParam.exportSubOptions(), 5, 7);
+        formatter.printOptions(writer, WRAP_WIDTH,
+                CommandLineParam.getOptions(CommandLineParam.REPORT), 2, 14);
+        formatter.printOptions(writer, WRAP_WIDTH,
+                CommandLineParam.reportSubOptions(), 5, 6);
     }
 }
