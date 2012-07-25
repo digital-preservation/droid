36d9a3f270ee30d0e1774cd7531915d75843f3d7
rbrennan <rbrennan@8cb85be0-0016-d946-ad3d-90027f349634>
2012-07-25 12:03:15 +0000

    Command line archive reporting
    git-svn-id: https://wb-d-tfs2.web.local/svn/dp/trunk/droid@199 8cb85be0-0016-d946-ad3d-90027f349634

---
+++ droid-command-line/src/main/java/uk/gov/nationalarchives/droid/command/archive/TarArchiveContentIdentifier.java
@@ -0,0 +1,101 @@
+/**
+ * <p>Copyright (c) The National Archives 2005-2010.  All rights reserved.
+ * See Licence.txt for full licence details.
+ * <p/>
+ *
+ * <p>DROID DCS Profile Tool
+ * <p/>
+ */
+package uk.gov.nationalarchives.droid.command.archive;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import uk.gov.nationalarchives.droid.command.ResultPrinter;
+import uk.gov.nationalarchives.droid.command.action.CommandExecutionException;
+import uk.gov.nationalarchives.droid.container.ContainerSignatureDefinitions;
+import uk.gov.nationalarchives.droid.core.BinarySignatureIdentifier;
+import uk.gov.nationalarchives.droid.core.interfaces.IdentificationRequest;
+import uk.gov.nationalarchives.droid.core.interfaces.IdentificationResultCollection;
+import uk.gov.nationalarchives.droid.core.interfaces.RequestIdentifier;
+import uk.gov.nationalarchives.droid.core.interfaces.resource.RequestMetaData;
+import uk.gov.nationalarchives.droid.core.interfaces.resource.TarEntryIdentificationRequest;
+
+/**
+ * Identifier for files held in a TAR archive
+ * 
+ * @author rbrennan
+ */
+public class TarArchiveContentIdentifier {
+
+    private BinarySignatureIdentifier binarySignatureIdentifier;
+    private ContainerSignatureDefinitions containerSignatureDefinitions;
+    private String path;
+    private String slash;
+    private File tmpDir;
+    
+    public TarArchiveContentIdentifier (BinarySignatureIdentifier binarySignatureIdentifier,
+            ContainerSignatureDefinitions containerSignatureDefinitions, String path, String slash) {
+    
+        synchronized(this) {
+            this.binarySignatureIdentifier = binarySignatureIdentifier;
+            this.containerSignatureDefinitions = containerSignatureDefinitions;
+            this.path = path;
+            this.slash = slash;
+            if(tmpDir == null) {
+                tmpDir = new File(System.getProperty("java.io.tmpdir"));
+            }
+        }
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void identify(URI uri, IdentificationRequest request)
+            throws CommandExecutionException, IOException {
+        
+        String newPath = "tar:" + path + request.getFileName() + "!" + slash;
+        InputStream tarIn = request.getSourceInputStream(); 
+        try {
+            TarArchiveInputStream in = new TarArchiveInputStream(tarIn);
+            try {
+                TarArchiveEntry entry = null; 
+                while ((entry = (TarArchiveEntry)in.getNextTarEntry()) != null) {
+                    String name = entry.getName();
+                    if (entry.isDirectory()) {
+//                        System.out.println(".......found directory: " + name);
+                    } else {
+//                        System.out.println(".......found file: " + name);
+                        RequestMetaData metaData = new RequestMetaData(1L, 2L, name);
+                        RequestIdentifier identifier = new RequestIdentifier(uri);
+                        TarEntryIdentificationRequest tarRequest =
+                            new TarEntryIdentificationRequest(metaData, identifier, tmpDir);
+                        try {
+                            tarRequest.open(in);
+                            IdentificationResultCollection tarResults =
+                                binarySignatureIdentifier.matchBinarySignatures(tarRequest);
+          //                  System.out.println("tarRequest results: " + tarResults.getResults().size());
+                            ResultPrinter resultPrinter =
+                                new ResultPrinter(binarySignatureIdentifier,
+                                    containerSignatureDefinitions, newPath, slash);
+                            resultPrinter.print(tarResults, tarRequest);
+                        } catch (IOException e) {
+                            throw new CommandExecutionException(e);
+                        }
+                    }
+                }
+            } finally {
+                if (in != null) {
+                    in.close();
+                }
+            }
+        } finally {
+            if (tarIn != null) {
+                tarIn.close();
+            }
+        }
+    }
+}
