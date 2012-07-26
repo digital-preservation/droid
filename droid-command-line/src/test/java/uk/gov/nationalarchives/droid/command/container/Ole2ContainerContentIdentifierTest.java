fe274bdf05907e8bf5838f33bfb0fc96ae511e39
rbrennan <rbrennan@8cb85be0-0016-d946-ad3d-90027f349634>
2012-07-26 14:14:43 +0000

    Container unit tests for no-profile command line
    git-svn-id: https://wb-d-tfs2.web.local/svn/dp/trunk/droid@215 8cb85be0-0016-d946-ad3d-90027f349634

---
+++ droid-command-line/src/test/java/uk/gov/nationalarchives/droid/command/container/Ole2ContainerContentIdentifierTest.java
@@ -0,0 +1,127 @@
+/**
+ * <p>Copyright (c) The National Archives 2005-2010.  All rights reserved.
+ * See Licence.txt for full licence details.
+ * <p/>
+ *
+ * <p>DROID DCS Profile Tool
+ * <p/>
+ */
+package uk.gov.nationalarchives.droid.command.container;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.Test;
+import uk.gov.nationalarchives.droid.command.action.CommandExecutionException;
+import uk.gov.nationalarchives.droid.container.*;
+import uk.gov.nationalarchives.droid.container.ole2.Ole2IdentifierEngine;
+import uk.gov.nationalarchives.droid.core.SignatureParseException;
+import uk.gov.nationalarchives.droid.core.interfaces.IdentificationResult;
+import uk.gov.nationalarchives.droid.core.interfaces.IdentificationResultCollection;
+import uk.gov.nationalarchives.droid.core.interfaces.RequestIdentifier;
+import uk.gov.nationalarchives.droid.core.interfaces.resource.FileSystemIdentificationRequest;
+import uk.gov.nationalarchives.droid.core.interfaces.resource.RequestMetaData;
+
+/**
+ *
+ * @author rbrennan
+ */
+public class Ole2ContainerContentIdentifierTest {
+    
+    private Ole2ContainerContentIdentifier ole2ContainerContentIdentifier;
+    private ContainerFileIdentificationRequestFactory requestFactory;
+    private Ole2IdentifierEngine ole2IdentifierEngine;
+    private ContainerIdentifierInit containerIdentifierInit;
+    private ContainerSignatureDefinitions containerSignatureDefinitions;
+    private Map<Integer, List<FileFormatMapping>> formats; 
+    private String containerSignatures =
+            "../droid-container/workDir/container-sigs/container-signature.xml";
+    private String ole2File =
+            "../droid-container/src/test/resources/word97.doc";
+    
+    @Before
+    public void setUp() {
+        ole2ContainerContentIdentifier = new Ole2ContainerContentIdentifier();
+        requestFactory = new ContainerFileIdentificationRequestFactory();
+        ole2IdentifierEngine = new Ole2IdentifierEngine();
+        containerIdentifierInit = new ContainerIdentifierInit();
+        formats = new HashMap<Integer, List<FileFormatMapping>>();
+    }
+    
+    @After
+    public void tearDown() {
+        ole2ContainerContentIdentifier = null;
+        requestFactory = null;
+        ole2IdentifierEngine = null;
+        containerIdentifierInit = null;
+        formats = null;
+    }
+    
+    @Test
+    public void identifyOle2FileTest() throws CommandExecutionException {
+
+        ole2IdentifierEngine.setRequestFactory(requestFactory);
+        ole2ContainerContentIdentifier.setIdentifierEngine(ole2IdentifierEngine);
+        Object object = ole2ContainerContentIdentifier.getIdentifierEngine();
+        assertEquals(object.getClass(), ole2IdentifierEngine.getClass());
+        
+        try {
+            InputStream in = new FileInputStream(containerSignatures);
+            ContainerSignatureSaxParser parser = new ContainerSignatureSaxParser();
+            containerSignatureDefinitions = parser.parse(in);
+        } catch (SignatureParseException e) {
+            throw new CommandExecutionException ("Can't parse container signature file");
+        } catch (Exception e) {
+            throw new CommandExecutionException(e);
+        }
+        
+        ole2ContainerContentIdentifier.init(containerSignatureDefinitions, "OLE2");
+        object = ole2ContainerContentIdentifier.getContainerIdentifierInit();
+        assertEquals(object.getClass(), containerIdentifierInit.getClass());
+        
+        object = ole2ContainerContentIdentifier.getFormats();
+        assertEquals(object.getClass(), formats.getClass());
+
+        String fileName;
+        File file = new File(ole2File);
+        if (!file.exists()) {
+            fail("OLE2 test file not found");
+        }
+        URI uri = file.toURI();
+        RequestIdentifier identifier = new RequestIdentifier(uri);
+        identifier.setParentId(1L);
+        try {
+            fileName = file.getCanonicalPath();
+            RequestMetaData metaData =
+                new RequestMetaData(file.length(), file.lastModified(), fileName);
+            FileSystemIdentificationRequest request =
+                new FileSystemIdentificationRequest(metaData, identifier);
+            IdentificationResultCollection results =
+                new IdentificationResultCollection(request);
+            InputStream ole2Stream = new FileInputStream(file);
+            request.open(ole2Stream);
+            results = ole2ContainerContentIdentifier.process(request.getSourceInputStream(), results);
+            if (results.getResults().isEmpty()) {
+                fail("OLE2 file not identified");
+            }
+            String fmtExpected = "";
+            for (IdentificationResult identResult : results.getResults()) {
+                if (identResult.getPuid().equals("fmt/40")) {
+                    fmtExpected = identResult.getPuid();
+                }
+            }
+            assertEquals(fmtExpected, "fmt/40");
+        } catch (IOException e) {
+            throw new CommandExecutionException(e);
+        }
+    }
+}
