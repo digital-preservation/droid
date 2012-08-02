6126592f92a2d02bf5d1992fb61cd863a7f60d72
rbrennan <rbrennan@8cb85be0-0016-d946-ad3d-90027f349634>
2012-08-02 14:56:13 +0000

    Add open-archives capability to no-profile command line
    git-svn-id: https://wb-d-tfs2.web.local/svn/dp/trunk/droid@236 8cb85be0-0016-d946-ad3d-90027f349634

--- droid-command-line/src/main/java/uk/gov/nationalarchives/droid/command/archive/ZipArchiveContentIdentifier.java
+++ droid-command-line/src/main/java/uk/gov/nationalarchives/droid/command/archive/ZipArchiveContentIdentifier.java
@@ -35,10 +35,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
-
 import uk.gov.nationalarchives.droid.command.ResultPrinter;
 import uk.gov.nationalarchives.droid.command.action.CommandExecutionException;
 import uk.gov.nationalarchives.droid.container.ContainerSignatureDefinitions;
@@ -50,7 +48,7 @@ import uk.gov.nationalarchives.droid.core.interfaces.resource.RequestMetaData;
 import uk.gov.nationalarchives.droid.core.interfaces.resource.ZipEntryIdentificationRequest;
 
 /**
- * Identifier for files held in a ZIP archive.
+ * Identifier for files held in a ZIP archive
  * 
  * @author rbrennan
  */
@@ -60,55 +58,64 @@ public class ZipArchiveContentIdentifier {
     private ContainerSignatureDefinitions containerSignatureDefinitions;
     private String path;
     private String slash;
+    private String slash1;
     private File tmpDir;
     
     /**
-     * @param binarySignatureIdentifier The Binary Signature Identifier
-     * @param containerSignatureDefinitions The Container Signature Definitions
-     * @param path The Path to the archive
-     * @param slash The slash character to use
+     * 
+     * @param binarySignatureIdentifier     binary signature identifier
+     * @param containerSignatureDefinitions container signatures
+     * @param path                          current archive path 
+     * @param slash                         local path element delimiter
+     * @param slash1                        local first container prefix delimiter
      */
-    public ZipArchiveContentIdentifier(final BinarySignatureIdentifier binarySignatureIdentifier,
-            final ContainerSignatureDefinitions containerSignatureDefinitions, final String path, final String slash) {
+    public ZipArchiveContentIdentifier (final BinarySignatureIdentifier binarySignatureIdentifier,
+            final ContainerSignatureDefinitions containerSignatureDefinitions,
+            final String path, final String slash, final String slash1) {
     
-        synchronized (this) {
+        synchronized(this) {
             this.binarySignatureIdentifier = binarySignatureIdentifier;
             this.containerSignatureDefinitions = containerSignatureDefinitions;
             this.path = path;
             this.slash = slash;
-            if (tmpDir == null) {
+            this.slash1 = slash1;
+            if(tmpDir == null) {
                 tmpDir = new File(System.getProperty("java.io.tmpdir"));
             }
         }
     }
     
-   /**
+    /**
      * @param uri The URI of the file to identify
      * @param request The Identification Request
      * @throws CommandExecutionException When an exception happens during execution
+     * @throws IOException When an exception happens during archive file access
      */
     public void identify(final URI uri, final IdentificationRequest request)
-        throws CommandExecutionException {
+            throws CommandExecutionException, IOException {
         
-        final String newPath = "zip:" + path + request.getFileName() + "!" + slash;
-        InputStream zipIn = null;
+        final String newPath = "zip:" + slash1 + path + request.getFileName() + "!" + slash;
+        slash1 = "";
+        InputStream zipIn = null; 
         try {
             zipIn = request.getSourceInputStream(); 
             final ZipArchiveInputStream in = new ZipArchiveInputStream(zipIn);
             try {
                 ZipArchiveEntry entry = null; 
-                while ((entry = (ZipArchiveEntry) in.getNextZipEntry()) != null) {
+                while ((entry = (ZipArchiveEntry)in.getNextZipEntry()) != null) {
                     final String name = entry.getName();
                     if (!entry.isDirectory()) {
                         final RequestMetaData metaData = new RequestMetaData(1L, 2L, name);
                         final RequestIdentifier identifier = new RequestIdentifier(uri);
                         final ZipEntryIdentificationRequest zipRequest =
                             new ZipEntryIdentificationRequest(metaData, identifier, tmpDir);
+                        
                         zipRequest.open(in);
                         final IdentificationResultCollection zipResults =
-                            binarySignatureIdentifier.matchBinarySignatures(zipRequest);
-                        final ResultPrinter resultPrinter = new ResultPrinter(
-                            binarySignatureIdentifier, containerSignatureDefinitions, newPath, slash);
+                                binarySignatureIdentifier.matchBinarySignatures(zipRequest);
+                        final ResultPrinter resultPrinter =
+                                new ResultPrinter(binarySignatureIdentifier,
+                                    containerSignatureDefinitions, newPath, slash, slash1, true);
                         resultPrinter.print(zipResults, zipRequest);
                     }
                 }
@@ -117,15 +124,9 @@ public class ZipArchiveContentIdentifier {
                     in.close();
                 }
             }
-        } catch (IOException e) {
-            throw new CommandExecutionException(e);
         } finally {
             if (zipIn != null) {
-                try {
-                    zipIn.close();
-                } catch (IOException ioe) {
-                    throw new CommandExecutionException(ioe);
-                }
+                zipIn.close();
             }
         }
     }
