a87e2e686155196894496c71b88940a40bda2fe5
snail1966 <snail1966@hotmail.com>
2013-06-10 13:04:47 +0100

    Change for Java 1.7 fix. Includes:- updated aspectJ refereence in droid-parent pom - now v1.7.2- updated signature files- corrections to character encoding in Help- updated version references in Help FAQ and txt file.- amended droid.sh file to remove Windows style line endings (which cause Bad Interpreter error on Linux/Mac OS X.

--- droid-core-interfaces/src/main/java/uk/gov/nationalarchives/droid/core/interfaces/config/DroidGlobalConfig.java
+++ droid-core-interfaces/src/main/java/uk/gov/nationalarchives/droid/core/interfaces/config/DroidGlobalConfig.java
@@ -68,8 +68,8 @@ public class DroidGlobalConfig {
     private static final String DEFAULT_DROID_PROPERTIES = "default_droid.properties";
 
     //FIXME: update to latest signature file before release.
-    private static final String DROID_SIGNATURE_FILE = "DROID_SignatureFile_V65.xml";
-    private static final String CONTAINER_SIGNATURE_FILE = "container-signature-20120828.xml";
+    private static final String DROID_SIGNATURE_FILE = "DROID_SignatureFile_V68.xml";
+    private static final String CONTAINER_SIGNATURE_FILE = "container-signature-20130501.xml";
     private static final String TEXT_SIGNATURE_FILE = "text-signature-20101101.xml";
     
     private static final String DATABASE_DURABILITY = "database.durability";
