230ec285616f66a2371218ca60c731a39d178f81
snail1966 <snail1966@hotmail.com>
2015-05-27 12:18:20 +0100

    Added new Left and Right Fragment classes for Byteseek 2 version which were missing from earlier commit

---
+++ droid-core/src/main/java/uk/gov/nationalarchives/droid/core/signature/droid6/LeftFragmentBS2.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2012, The National Archives <pronom@nationalarchives.gsi.gov.uk>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of the The National Archives nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/*
+ * The National Archives 2005-2006.  All rights reserved.
+ * See Licence.txt for full licence details.
+ *
+ * Developed by:
+ * Tessella Support Services plc
+ * 3 Vineyard Chambers
+ * Abingdon, OX14 3PX
+ * United Kingdom
+ * http://www.tessella.com
+ *
+ * Tessella/NPD/4305
+ * PRONOM 4
+ *
+ * $History: LeftFragment.java $
+ * 
+ * *****************  Version 3  *****************
+ * User: Walm         Date: 20/06/05   Time: 9:58
+ * Updated in $/PRONOM4/FFIT_SOURCE/signatureFile
+ * Returns a blank string for toString()
+ * 
+ * *****************  Version 2  *****************
+ * User: Walm         Date: 5/04/05    Time: 18:07
+ * Updated in $/PRONOM4/FFIT_SOURCE/signatureFile
+ * review headers
+ *
+ */
+package uk.gov.nationalarchives.droid.core.signature.droid6;
+
+/**
+ * A left fragment is all the parts of a byte sequence to
+ * the left of an anchoring subsequence.  These parts cannot
+ * be searched for using the fast BoyerMooreHorspool algorithm
+ * so their existence is checked for once an anchoring sequence 
+ * has been found.
+ * 
+ * <p/>This subclass of SideFragment is only required so the XML
+ * parser can build an instance of this class and assemble the
+ * left fragments together.
+ * 
+ * <p/>Left and Right Fragments are otherwise identical in functionality.
+ * 
+ * @author Martin Waller
+ * @author Matt Palmer.
+ * @version 6.0.0
+ */
+public class LeftFragmentBS2 extends SideFragmentBS2 {
+
+}
