1f2cd1c9e05e7c63d3cfbe5629ad1782e92eba37
Matt Palmer <mattpalms@gmail.com>
2015-07-12 15:46:01 +0100

    Add more comments and TODOs.

--- droid-results/src/main/java/uk/gov/nationalarchives/droid/submitter/FileEventHandler.java
+++ droid-results/src/main/java/uk/gov/nationalarchives/droid/submitter/FileEventHandler.java
@@ -100,6 +100,16 @@ public class FileEventHandler {
         identifier.setParentResourceId(parentId);
         identifier.setResourceId(nodeId);
 
+
+        //TODO: must stop factories opening the request.  Important to be able to get a request
+        //      object to carry the id, even if it subsequently fails opening it.  This is
+        //      handled differently by result handlers (insert vs. update).
+
+        //TODO: this assumes you can handle an error *after* you have already submitted the file
+        //      for processing.  This works with the hibernate style entity manager, which will
+        //      update the entity if its properties change subsequently.  It's problematic if
+        //      we're using a more traditional database system. OR - we need to have an update
+        //      mechanism for errors, which updates an existing node status when an error occurs.
         try {
             droidCore.submit(requestFactory.newRequest(metaData, identifier, file));
             submissionThrottle.apply();
@@ -112,6 +122,7 @@ public class FileEventHandler {
                 log.warn(String.format("File not found: [%s]", file.getAbsolutePath()));
             }
             //TODO: ugly to create a new request when the factory fails to open it for error reporting.
+            //      and will miss any id assigned to the request if saved.
             final FileSystemIdentificationRequest request = new FileSystemIdentificationRequest(metaData, identifier);
             resultHandler.handleError(new IdentificationException(request, error, e));
         } catch (InterruptedException e) {
