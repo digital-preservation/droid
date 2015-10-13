6f49d050e72aeb9d972901ce978454e10157a24b
Brian O'Reilly <snail1966@hotmail.com>
2015-10-13 21:29:54 +0100

    setNullableTimeStamp minor manual edit in JDBCBatchResultHandlerDAO

--- droid-results/src/main/java/uk/gov/nationalarchives/droid/results/handlers/JDBCBatchResultHandlerDao.java
+++ droid-results/src/main/java/uk/gov/nationalarchives/droid/results/handlers/JDBCBatchResultHandlerDao.java
@@ -651,7 +651,6 @@ public class JDBCBatchResultHandlerDao implements ResultHandlerDao {
             final PreparedStatement insertNode = insertNodeStatement;
             insertNode.setLong(1, nodeId);
             insertNode.setBoolean(2, mismatch);
-            //insertNode.setDate(3, finished);
             SqlUtils.setNullableTimestamp(3, finished, insertNode);
             SqlUtils.setNullableInteger(   4,  numIdentifications, insertNode);
             SqlUtils.setNullableString(    5,  extension, insertNode);
