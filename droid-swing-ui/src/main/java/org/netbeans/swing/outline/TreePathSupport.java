a861b917611f9ba891a56ab46ff9d8fa2fce1fc3
Jeremie <charlet.jeremie@gmail.com>
2019-11-20 15:43:44 +0200

    upd: override TreePathSupport to ensure that the EventBroadcaster listener always executes last (the listener used internally by the swing outline to fire table model events on tree events) see https://github.com/digital-preservation/droid/issues/306#issuecomment-555434012

--- droid-swing-ui/src/main/java/org/netbeans/swing/outline/TreePathSupport.java
+++ droid-swing-ui/src/main/java/org/netbeans/swing/outline/TreePathSupport.java
@@ -1,20 +1,33 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+/**
+ * Copyright (c) 2016, The National Archives <pronom@nationalarchives.gsi.gov.uk>
+ * All rights reserved.
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of the The National Archives nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.netbeans.swing.outline;
 
@@ -51,6 +64,16 @@ import java.util.List;
  * Its <code>addTreeWillExpandListener</code> code supports 
  * <code>ExtTreeWillExpandListener</code>, so such a listener may be notified
  * if some other listener vetoes a pending expansion event.
+ *
+ * <br/>
+ * <br/>
+ * <p>
+ *     Hot fix for issue mentioned in https://github.com/digital-preservation/droid/issues/306#issuecomment-555434012
+ *
+ *     to ensure that the EventBroadcaster listener (the listener used internally by the swing outline to fire table model events on tree events) always executes last.
+ *
+ * This then means that if any previous listeners modify the tree state, it will see the true state of the tree before issuing update events.
+ * </p>
  *
  * @author  Tim Boudreau
  */
@@ -290,7 +313,8 @@ public final class TreePathSupport {
      * @param l The tree will expand listener
      */
     public synchronized void addTreeWillExpandListener (TreeWillExpandListener l) {
-        weListeners.add(l);
+        weListeners.add(0, l); // insert new listeners at the start.  This ensures the outline listener is always the last listener.
+
     }
     
     /**
