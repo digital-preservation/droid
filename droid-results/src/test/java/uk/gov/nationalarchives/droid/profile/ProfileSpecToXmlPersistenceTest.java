c9678e7b5cb350552aa392b8c349996a3f9bfb19
sparkhi <parkhi@gmail.com>
2020-01-30 14:49:11 +0000

    Updated a flaky test and added cleanup code

--- droid-results/src/test/java/uk/gov/nationalarchives/droid/profile/ProfileSpecToXmlPersistenceTest.java
+++ droid-results/src/test/java/uk/gov/nationalarchives/droid/profile/ProfileSpecToXmlPersistenceTest.java
@@ -36,7 +36,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TimeZone;
 
 import javax.xml.bind.JAXBException;
 
@@ -44,8 +47,10 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.apache.commons.io.FileUtils;
 import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.Difference;
+import org.custommonkey.xmlunit.DifferenceListener;
+import org.custommonkey.xmlunit.NodeDetail;
 import org.custommonkey.xmlunit.XMLAssert;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.joda.time.DateTime;
@@ -54,14 +59,15 @@ import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.After;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
+import org.junit.rules.TemporaryFolder;
+import org.w3c.dom.Node;
 import uk.gov.nationalarchives.droid.core.interfaces.filter.CriterionFieldEnum;
 import uk.gov.nationalarchives.droid.core.interfaces.filter.CriterionOperator;
 import uk.gov.nationalarchives.droid.core.interfaces.filter.FilterValue;
-import uk.gov.nationalarchives.droid.util.FileUtil;
 
 /**
  * @author rflitcroft
@@ -73,8 +79,8 @@ public class ProfileSpecToXmlPersistenceTest {
     public static final String TEST_DATE = "2009-01-01 12:00:00 GMT";
     private static final TimeZone TEST_TIME_ZONE = TimeZone.getTimeZone("Europe/London");
 
-    Path tmpDir;
-	Path profileFile;
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
     private JaxbProfileSpecDao profileSpecJaxbDao;
 
@@ -95,28 +101,13 @@ public class ProfileSpecToXmlPersistenceTest {
     @Before
     public void setup() throws JAXBException, IOException {
         profileSpecJaxbDao = new JaxbProfileSpecDao();
-        Files.createDirectories(Paths.get("profiles/untitled-1"));
-        tmpDir = Paths.get("tmp");
-        Files.createDirectories(tmpDir);
-        profileFile = tmpDir.resolve("profile.xml");
     }
 
-    @After
-    public void tearDown() throws IOException {
-       FileUtil.deleteQuietly(Paths.get("profiles"));
-       if(Files.exists(profileFile)) {
-    	   profileFile.toFile().deleteOnExit();
-       }
-       if(Files.exists(tmpDir)) {
-    	   tmpDir.toFile().deleteOnExit();
-       }
-    }
-    
+
     @Test
     public void testSaveEmptyProfileSpecAsXml() throws Exception {
-
-        File file = new File("profiles/untitled-1/profile.xml");
-        FileUtils.deleteQuietly(file);
+        File profilesFolder = temporaryFolder.newFolder("profiles/untitled-1");
+        File file = new File(profilesFolder.getAbsolutePath() +  "/profile.xml");
 
         ProfileSpec profileSpec = new ProfileSpec();
 
@@ -127,7 +118,7 @@ public class ProfileSpecToXmlPersistenceTest {
         profile.setDateCreated(DATE_FORMAT.parse(TEST_DATE));
         profile.setSignatureFileVersion(26);
 
-        profileSpecJaxbDao.saveProfile(profile, Paths.get("profiles/untitled-1"));
+        profileSpecJaxbDao.saveProfile(profile, profilesFolder.toPath());
 
         String control = "<Profile Id=\"untitled-1\">"
                 + "  <CreatedDate>2009-01-01T00:00:00Z</CreatedDate>"
@@ -141,14 +132,12 @@ public class ProfileSpecToXmlPersistenceTest {
 
         Reader test = new FileReader(file);
         XMLAssert.assertXMLEqual(new StringReader(control), test);
-
     }
 
     @Test
     public void testSaveProfileSpecWithSomeResources() throws Exception {
-
-        final Path file = Paths.get("profiles/untitled-1/profile.xml");
-        FileUtil.deleteQuietly(file);
+        File profilesFolder = temporaryFolder.newFolder("profiles/untitled-1");
+        final Path file = new File(profilesFolder.getAbsolutePath() +  "/profile.xml").toPath();
 
         final Path resource1 = Paths.get("file/1");
         final Path resource2 = Paths.get("file/2");
@@ -168,7 +157,7 @@ public class ProfileSpecToXmlPersistenceTest {
         profile.setProfileSpec(profileSpec);
         profile.setDateCreated(DATE_FORMAT.parse(TEST_DATE));
 
-        profileSpecJaxbDao.saveProfile(profile, Paths.get("profiles/untitled-1"));
+        profileSpecJaxbDao.saveProfile(profile, profilesFolder.toPath());
 
         DateTime testDateTime = new DateTime(0L);
         DateTimeFormatter formatter = ISODateTimeFormat.dateTimeNoMillis().withZone(DateTimeZone.forTimeZone(TEST_TIME_ZONE));
@@ -221,7 +210,7 @@ public class ProfileSpecToXmlPersistenceTest {
         }
     }
 
-    public String getPath(final Path file) {
+    private String getPath(final Path file) {
         final String location = file.toUri().toString();
         final String decodedLocation = java.net.URLDecoder.decode(location);
         final int uriPrefix = decodedLocation.indexOf(":/");
@@ -235,7 +224,11 @@ public class ProfileSpecToXmlPersistenceTest {
         final Path resource3 = Paths.get("dir/1");
         final Path resource4 = Paths.get("dir/2");
 
-        try(final BufferedWriter writer = Files.newBufferedWriter(Paths.get("profiles/untitled-1/profile.xml"), UTF_8)) {
+        File profilesFolder = temporaryFolder.newFolder("profiles/untitled-1");
+        File file = new File(profilesFolder.getAbsolutePath() +  "/profile.xml");
+        file.createNewFile();
+
+        try(final BufferedWriter writer = Files.newBufferedWriter(file.toPath(), UTF_8)) {
             writer.append("<Profile>");
             writer.newLine();
             writer.append("  <CreatedDate>2009-07-01T00:00:00Z</CreatedDate>");
@@ -318,8 +311,7 @@ public class ProfileSpecToXmlPersistenceTest {
         }
 
         ProfileInstance profile = profileSpecJaxbDao
-                .loadProfile(new FileInputStream(
-                        "profiles/untitled-1/profile.xml"));
+                .loadProfile(new FileInputStream(file.getAbsolutePath()));
 
         assertEquals("STOPPED", profile.getState().name());
         assertEquals(120, profile.getThrottle());
@@ -347,7 +339,7 @@ public class ProfileSpecToXmlPersistenceTest {
     
     @Test
     public void testSaveProfileSpecWithFilter() throws Exception {
-        
+
         FilterCriterionImpl criterion = new FilterCriterionImpl();
         criterion.setField(CriterionFieldEnum.PUID);
         criterion.setOperator(CriterionOperator.ANY_OF);
@@ -357,13 +349,12 @@ public class ProfileSpecToXmlPersistenceTest {
         FilterImpl filter = new FilterImpl();
         filter.setEnabled(true);
         filter.addFilterCiterion(criterion, 0);
-        
+
         ProfileInstance profile = new ProfileInstance(ProfileState.INITIALISING);
         profile.changeState(ProfileState.STOPPED);
         profile.setFilter(filter);
-        
-        profileSpecJaxbDao.saveProfile(profile, tmpDir);
 
+        profileSpecJaxbDao.saveProfile(profile, temporaryFolder.getRoot().toPath());
         String control = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"
             + "<Profile>"
             + "    <CreatedDate>"
@@ -389,7 +380,36 @@ public class ProfileSpecToXmlPersistenceTest {
             + "    <Throttle>0</Throttle>"
             + "</Profile>";
 
-        XMLAssert.assertXMLEqual(new StringReader(control), new FileReader("tmp/profile.xml"));
+        Diff difference = XMLUnit.compareXML(new StringReader(control), new FileReader(temporaryFolder.getRoot() + "/profile.xml"));
+        difference.overrideDifferenceListener(new IgnoreMillisecondsDifferenceInDateString());
+        assertTrue(difference.toString(), difference.identical());
     }
-    
+
+    /**
+     * This class implements the DifferenceListener and ignores the differences found in the milliseconds
+     * part of the CreatedDate
+     * e.g. If by chance, the profile is saved exactly at 0 millisecondd, we were facing error message like
+     *      "Expected text value '2020-01-30T11:59:20.000Z' but was '2020-01-30T11:59:20Z'"
+     */
+    private class IgnoreMillisecondsDifferenceInDateString implements DifferenceListener {
+        @Override
+        public int differenceFound(Difference difference) {
+            NodeDetail controlNode = difference.getControlNodeDetail();
+            NodeDetail testNode = difference.getControlNodeDetail();
+
+            if (controlNode.getXpathLocation().equals(testNode.getXpathLocation())) {
+                if ("CreatedDate".equals(controlNode.getNode().getParentNode().getNodeName())) {
+                    if (controlNode.getValue().equals(testNode.getValue())) {
+                        return RETURN_IGNORE_DIFFERENCE_NODES_IDENTICAL;
+                    }
+                }
+            }
+            return RETURN_IGNORE_DIFFERENCE_NODES_SIMILAR;
+        }
+
+        @Override
+        public void skippedComparison(Node node, Node node1) {
+            throw new UnsupportedOperationException("not supported");
+        }
+    }
 }
