90d999806c2638b4ade731683d9a1874e910637d
sparkhi <parkhi@gmail.com>
2024-01-26 18:34:10 +0000

    Updated to get name from binary signature file based on puid when identification method is container

--- droid-core/src/main/java/uk/gov/nationalarchives/droid/core/BinarySignatureIdentifier.java
+++ droid-core/src/main/java/uk/gov/nationalarchives/droid/core/BinarySignatureIdentifier.java
@@ -254,5 +254,15 @@ public class BinarySignatureIdentifier implements DroidCore {
             }
         }
     }
-   
+
+    /**
+     * Return the name of format based on the puid.
+     * @param puid
+     * @return format name
+     */
+    @Override
+    public String formatNameByPuid(String puid) {
+        FileFormat format = sigFile.getFileFormat(puid);
+        return format != null? format.getName() : null;
+    }
 }
