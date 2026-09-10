997ef60889e1f7bbd21d670e0b0ecf805baf14b9
Saurabh Parkhi <4743002+sparkhi@users.noreply.github.com>
2026-09-10 09:16:18 +0100

    Log URI in ItemWriterImplTest (#1702)
    * Log URI in ItemWriterImplTest
    
    * Made to use Junit4 provider and changes to fix any surfaced issues
    
    * merged removal of joda-time from another branch
    
    * converted test to not use joda time
    
    * Windows does not support trailing spaces.
    
    * Windows does not support trailing spaces.
    
    ---------
    
    Co-authored-by: sparkhi <sparkhi@users.noreply.github.com>

--- droid-results/src/test/java/uk/gov/nationalarchives/droid/submitter/ProfileSpecWalkerImplTest.java
+++ droid-results/src/test/java/uk/gov/nationalarchives/droid/submitter/ProfileSpecWalkerImplTest.java
@@ -36,6 +36,7 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -182,7 +183,11 @@ public class ProfileSpecWalkerImplTest {
 
         for (AbstractProfileResource resource : s3Resources) {
             String key = resource.getName().startsWith("/") ? resource.getName().substring(1) : resource.getName();
-            S3Object s3Object = S3Object.builder().key(key).build();
+            S3Object s3Object = S3Object.builder()
+                    .key(key)
+                    .lastModified(Instant.parse("2024-06-06T00:00:00Z"))
+                    .size(1L)
+                    .build();
             ArgumentMatcher<ListObjectsV2Request> requestArgumentMatcher = argument ->
                     argument != null && resource.getName().equals(argument.prefix());
             ListObjectsV2Response response = ListObjectsV2Response.builder().contents(List.of(s3Object)).build();
